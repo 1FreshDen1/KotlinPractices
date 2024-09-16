934cc6587d0d9db888721f548b91e2e6ed6338e9
Denis Kochelaev <kochelaev.denisss@gmail.com>
2024-09-16 23:07:56 +0300

    Second commit

---
+++ Practice2/app/src/main/java/com/example/practice2/ThirdFragment.kt
@@ -0,0 +1,24 @@
+package com.example.practice2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.practice2.databinding.FragmentThirdBinding
+
+class ThirdFragment : Fragment() {
+    private lateinit var binding: FragmentThirdBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentThirdBinding.inflate(inflater, container, false)
+
+        binding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+        return binding.root
+    }
+}
